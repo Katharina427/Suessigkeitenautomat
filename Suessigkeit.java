7618615e8d1f7c5618945d601e6b133292ce7133
Katharina427 <betzkatharina3@gmail.com>
2025-06-03 14:59:47 +0200

    Klasse Suessigkeit erstellt erste Attribute Konstruktor

--- Suessigkeit.java
+++ Suessigkeit.java
@@ -2,4 +2,10 @@ public class Suessigkeit {
 
     private String farbe;
     private boolean vegan;
+
+
+    public Suessigkeit(String farbe, boolean vegan) {
+        this.farbe = farbe;
+        this.vegan = vegan;
+    }
 }
